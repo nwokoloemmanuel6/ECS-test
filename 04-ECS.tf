7fdd1cff3165c649db94a77ca78c31dca87ed57f
Nwokolo Emmanuel <nwokoloebuka84@gmail.com>
2023-06-16 08:29:26 +0000

    changed cpu and memory

--- 04-ECS.tf
+++ 04-ECS.tf
@@ -100,8 +100,8 @@ resource "aws_ecs_task_definition" "aws-ecs-task" {
 
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  memory                   = "512"
-  cpu                      = "256"
+  memory                   = "1024"
+  cpu                      = "512"
   execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
   task_role_arn            = aws_iam_role.ecsTaskExecutionRole.arn
 
