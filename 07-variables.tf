b5b8d1738c0952e8ca57e16b62593a1ea442f0f4
nwokolo emmanuel <99542337+nwokoloemmanuel6@users.noreply.github.com>
2023-06-16 08:36:21 +0100

    Update 07-variables.tf
    removed access key variable

--- 07-variables.tf
+++ 07-variables.tf
@@ -1,13 +1,3 @@
-variable "aws_access_key" {
-  type        = string
-  description = "AWS Access Key"
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS Secret Key"
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS Region"
